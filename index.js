a60f3e4ec11efbb6bc8b4ab794be79e2756018fc
Roberto Jacquez <rjqz92@gmail.com>
2021-11-05 17:17:47 -0600

    commit tree 4bcace33333b73309fd0119eaaac78cc854fa612

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+function superbowlWin(records){
+    const yearFound = records.find(record => record.result === "W")
+    if (yearFound){
+        return yearFound.year;
+    }
+    
+}
 
+
+/*function superbowlWin(array) {
+    const found = array.find(object => 
+    // insert condition to match 
+    //use dot notation to check the year property of object)
+
+    return found
+} 
+*/
